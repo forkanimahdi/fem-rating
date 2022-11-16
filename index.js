cdb3628496a1c6d48f5c0c10b50c9d617974a536
forkani <96060207+forkanimahdi@users.noreply.github.com>
2022-11-16 14:31:28 +0000

    commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const body = document.getElementById("body")
+const buttonsOfRating = document.querySelectorAll(".ratingbutton")
+const submiting = document.getElementById("submit")
+const ratingPhse = document.getElementById("ratingphase");
+const thanksPhase = document.getElementById("thanksphase");
+const ratingNumber = document.getElementById('ratingnumber')
+
+buttonsOfRating.forEach(rate => (rate.addEventListener("click" , () => selectit(rate))))
+
+
+function selectit(buttonOfRating){
+  const text = buttonOfRating.textContent
+  if(text == "1"||text == "2"||text == "3"||text == "4"||text == "6" ){
+ratingNumber.textContent = text
+submiting.addEventListener("click" , () => submitratig())
+
+}
+
+else{
+  console.log("jjj")
+}
+function submitratig(){
+
+  ratingPhse.style.display = 'none'
+  thanksPhase.style.visibility = 'visible'
+  
+  
+  }
+}
